31b1c4e0d7a56c0026f3496d404dea0d96c26405
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-04-19 10:46:37 -0400

    Update portfolio.js
    checking webhook status

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,7 +11,7 @@ const seo = {
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Prakash Upadhyay Portfolio",
+    title: "Kirthikraj Kamaraj Portfolio",
     type: "website",
     url: "https://prakashupadhyay.netlify.app/",
   },
