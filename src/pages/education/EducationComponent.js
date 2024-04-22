e46af6dbfe5b6fefebe66cdb4e7c75eedcb1e6aa
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-04-22 08:20:41 -0400

    Update EducationComponent.js
    updated the education intro section

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -34,7 +34,7 @@ class Education extends Component {
                 <h3 className="heading-sub-text" style={{ color: theme.text }}>
                   Basic Qualification and Certifcations
                 </h3>
-               <p>I am currently pursuing a Master of Science in Computer Science at Stony Brook University, NY, USA, with my graduation expected in December 2024. My studies focus on Theory of Database Systems, Data Science Fundamentals, and Advances in AI/ML. Before this, I completed my Bachelor of Engineering in Information Technology from University of Mumbai, India, achieving a CGPA of 9.3/10. My undergraduate coursework included Data Structures, Design and Analysis of Algorithms, Database Management System, Object-Oriented Programming, Java, Operating Systems, Python Programming, and Software Engineering.</p>
+               <p>I am currently pursuing a Master of Science in Computer Science at Binghamton University, NY, USA, with my graduation expected in May 2024. My studies focus on Cloud Computing, Database Systems. Before this, I completed my Bachelor of Engineering in Computer Science from University of Mumbai, India, achieving a CGPA of 8.43/10. My undergraduate coursework included Data Structures, Design and Analysis of Algorithms, Database Management System, Object-Oriented Programming, Java, Operating Systems, Python Programming, and Software Engineering.</p>
               </div>
             </div>
           </Fade>
